ff40306b6a20ad3615aa9981c370c848233e9b2c
grazzel.mondonedo@gmail.com <grazzel.mondonedo@gmail.com>
2019-04-05 13:16:58 +0800

    adds a comment to recipes/default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,3 +4,13 @@
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
 # This is my second attemp at using nano
+# This is a push to github test.
+package 'httpd'
+
+file '/var/www/html/index.html' do
+  content '<h1>Hello, world!</h1>'
+end
+
+service 'httpd' do
+  action [:enable, :start]
+end
